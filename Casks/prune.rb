cafd3cb9819a3342dfb4df20222cb402338d6fb5
Ryan Schumacher <j.r.schumacher@gmail.com>
2026-02-23 17:46:04 -0500

    Update prune cask to 1.0.0-rc.2 (use homebrew-tap releases)

--- Casks/prune.rb
+++ Casks/prune.rb
@@ -2,7 +2,7 @@ cask "prune" do
   version "1.0.0-rc.2"
   sha256 "7d75d6178c99de5b3c8218f82458559fd3bda5283cd2fc9b07f077883a3bde3e"
 
-  url "https://github.com/jrschumacher/prune/releases/download/v#{version}/Prune_App_macOS_universal.zip"
+  url "https://github.com/jrschumacher/homebrew-tap/releases/download/v#{version}/Prune_App_macOS_universal.zip"
   name "Prune"
   desc "Personal task and work-log tool built around capture, chronology, and forgetting"
   homepage "https://github.com/jrschumacher/prune"
