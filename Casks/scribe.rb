79c85b8ad2d46daaa544dbf47fd80f5578769ab6
Ryan Schumacher <j.r.schumacher@gmail.com>
2026-03-08 10:44:14 -0500

    fix: update scribe cask with sha256

--- Casks/scribe.rb
+++ Casks/scribe.rb
@@ -1,13 +1,13 @@
 cask "scribe" do
   version "0.1.0"
-  sha256 :no_check
+  sha256 "02a5441832683cc792928a01b91e870e3de53295972d386c6a8bc9797c0f9bce"
 
   url "https://github.com/jrschumacher/homebrew-tap/releases/download/scribe-v#{version}/Scribe_macOS_universal.zip"
   name "Scribe"
   desc "Audio transcription app powered by Deepgram"
   homepage "https://github.com/jrschumacher/scribe"
 
-  app "Scribe.app"
+  app "scribe.app"
 
   zap trash: [
     "~/Library/Application Support/Scribe",
