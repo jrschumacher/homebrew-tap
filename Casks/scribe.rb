57d2481e65a26a9831d41937b9be98e1329c02b0
Ryan Schumacher <j.r.schumacher@gmail.com>
2026-03-15 21:27:36 -0500

    Update scribe to v1.1.1 (arm64)

--- Casks/scribe.rb
+++ Casks/scribe.rb
@@ -1,13 +1,13 @@
 cask "scribe" do
   version "1.1.1"
-  sha256 "d4fb1aee5fd8f438dc72431e514389c62e5e74daadc982ee3dbd38edb0a56e84"
+  sha256 "dfd9c2f19c4848b222403e4c15d37b88bd37f579e53c2dd03f7e405a7ff88c5d"
 
-  url "https://github.com/jrschumacher/homebrew-tap/releases/download/scribe-v#{version}/Scribe_macOS_universal.zip"
+  url "https://github.com/jrschumacher/homebrew-tap/releases/download/scribe-v1.1.1/scribe-1.1.1-darwin-arm64.zip"
   name "Scribe"
   desc "Audio transcription app powered by Deepgram"
   homepage "https://github.com/jrschumacher/scribe"
 
-  app "scribe.app"
+  app "Scribe.app"
 
   zap trash: [
     "~/Library/Application Support/Scribe",
