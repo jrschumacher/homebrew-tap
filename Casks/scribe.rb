7302b9b3dc3d4bf8eb5ddde5f7a0fe14f53de759
Ryan Schumacher <j.r.schumacher@gmail.com>
2026-03-15 21:23:27 -0500

    Update scribe to v1.1.1 (arm64)

--- Casks/scribe.rb
+++ Casks/scribe.rb
@@ -1,6 +1,6 @@
 cask "scribe" do
-  version "1.1.0"
-  sha256 "c4aed4ac283b09060073e865abab327df0a4bd48b32dbfc6dafbb6e498314f15"
+  version "1.1.1"
+  sha256 "d4fb1aee5fd8f438dc72431e514389c62e5e74daadc982ee3dbd38edb0a56e84"
 
   url "https://github.com/jrschumacher/homebrew-tap/releases/download/scribe-v#{version}/Scribe_macOS_universal.zip"
   name "Scribe"
